7b8bcc041217d8de3506fd406538b61327d05810
DvpsPranavi <dvpspranavi.bsc23@rvu.edu.in>
2024-02-06 15:21:22 +0530

    debug02

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,2 +1,10 @@
 //2. Write a C program to add two numbers.
+#include<stdio.h>
+int main(void)
+{
+    int x=4,y=5,sum;
+    sum=x+y;
+    printf("The sum of the two numbers are %d\n",sum);
+    return 0;
+}
 
