c7cbb6c5a2b777a7ac5040038f3c37d2b4ad85c3
DvpsPranavi <dvpspranavi.bsc23@rvu.edu.in>
2024-02-12 14:05:48 +0530

    debug07

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,1 +1,53 @@
+//5. Write a C program to compare three numbers using **pass by value**
+#include <stdio.h>
+int input();
+int compare(int a, int b, int c);
+void output(int a, int b, int c, int largest);
 
+int main()
+{
+    int a,b,c,largest;
+    a=input();
+    b=input();
+    c=input();
+    largest=compare(a,b,c);
+    output(a,b,c,largest);
+    return 0;
+}
+
+int input()
+{
+    int n;
+    printf("Enter the number\n");
+    scanf("%d",&n);
+    return n;
+}
+
+int compare(int a, int b, int c)
+{
+    int i,n;
+    for(i=0;i<n;i++)
+    {
+        if(a>=b &&a>=c)
+        {
+            return a;
+        } 
+
+        if( b>=c)
+        {
+            return b;
+        }  
+
+        else
+        {
+            return c;
+        }
+    }
+
+    return 0;
+}
+
+void output(int a, int b, int c, int largest)
+{
+    printf("The largest number between %d,%d and %d is %d\n",a,b,c,largest);
+}
---
+++ set01/problem07.c
@@ -0,0 +1,38 @@
+//7. Write a C program to find sum of all natural numbers until _n_
+#include <stdio.h>
+int input_n();
+int sum_n_nos(int n);
+void output(int n, int sum);
+
+int main()
+{
+    int n,sum=0;
+    n=input_n();
+    sum=sum_n_nos(n);
+    output(n,sum);
+    return 0;
+}
+
+int input_n()
+{
+    int x;
+    printf("Enter the number of natural numbers to be added");
+    scanf("%d",&x);
+    return x;
+}
+
+int sum_n_nos(int n)
+{
+    int sum=0;
+    for(int i =1; i<= n;i++)
+    {
+        sum += i;
+    }
+    return sum;
+
+}
+
+void output(int n, int sum)
+{
+    printf("The sum of the first %d natural numbers is %d \n",n,sum);
+}
