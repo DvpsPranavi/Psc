009728aa3e08879d19d305ce689a9ff54b7931c0
DvpsPranavi <dvpspranavi.bsc23@rvu.edu.in>
2024-02-16 14:26:25 +0530

    debug01

--- set02/problem01.c
+++ set02/problem01.c
@@ -1,1 +1,28 @@
 //01.  Write a program to find the area of a triangle.
+#include<stdio.h>
+void input(float *base,float *height)
+{
+    printf("Enter the base of the triangle\n");
+    scanf("%f",base);
+    printf("Enter the height of the triangle\n");
+    scanf("%f",height);
+}
+
+void find_area(float base , float height, float *area)
+{
+     *area=0.5*base*height;
+}
+
+
+void output(float base, float height, float area)
+{
+    printf("The area of the triangle with base %f ,height %f is %f\n",base,height,area);
+}
+int main()
+{
+    float base,height,area;
+    input(&base,&height);
+    find_area(base,height,&area);
+    output(base,height,area);
+    return 0;
+}
