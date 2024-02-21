07af0cb71838e0e364da8300397360f8979ebb60
DvpsPranavi <dvpspranavi.bsc23@rvu.edu.in>
2024-02-21 20:09:20 +0530

    debug02.6

---
+++ set02/problem06.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+void input_string(char *a);
+void str_reverse(char *str, char *rev_str);
+void output(char *a, char *reverse_a);
+
+int main() {
+    char input[100];  
+    char reversed[100];
+
+    input_string(input);
+    str_reverse(input, reversed);
+    output(input, reversed);
+
+    return 0;
+}
+
+
+void input_string(char *a) 
+{
+    printf("Enter a string: ");
+    scanf("%s", a);
+}
+
+// Function to reverse a string
+void str_reverse(char *str, char *rev_str) {
+    int length = 0;
+    while (str[length] != '\0') 
+    {
+        length++;
+    }
+
+    int i, j;
+    for (i = length - 1, j = 0; i >= 0; i--, j++) 
+    {
+        rev_str[j] = str[i];
+    }
+    rev_str[j] = '\0';
+}
+
+void output(char *a, char *reverse_a) {
+    printf("Original String is %s\n", a);
+    printf("Reversed String is %s\n", reverse_a);
+}
