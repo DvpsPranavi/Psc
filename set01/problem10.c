3dd8582b1cc0c0954c840060e8676d39ea2b9b62
DvpsPranavi <dvpspranavi.bsc23@rvu.edu.in>
2024-02-24 21:24:59 +0530

    debug10

--- set01/problem10.c
+++ set01/problem10.c
@@ -25,7 +25,7 @@ void input_two_strings(char *string1, char *string2)
 int stringcompare(char *string1, char *string2)
 {
     
-    for(int i=0;string1[i]!='\0' || string2!='\0';i++)
+    for(int i=0;string1[i]!='\0' && string2!='\0';i++)
     {
         
         if(string1[i]<string2[i])
